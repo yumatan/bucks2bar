bee9141ba7cd668b3e90bfa5a33d499a33155f5e
yumatan <yuma.tanaka1129@gmail.com>
2024-02-22 20:41:37 +0900

    add: empty chart

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
-// This file is intentionally left blank
+var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        datasets: [{
+            label: 'Income',
+            data: [/* income data here */],
+            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+            borderColor: 'rgba(75, 192, 192, 1)',
+            borderWidth: 1
+        },
+        {
+            label: 'Expenses',
+            data: [/* expenses data here */],
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgba(255, 99, 132, 1)',
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
